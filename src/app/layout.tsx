c85940dccad1af54a3eb8a27fd1b9b8ae1af1358
Ayush Tiwari <01tiwari.ayush@gmail.com>
2025-03-28 03:14:17 +0530

    [NS-1]: Base Setup (#1)

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next';
+import './globals.css';
+import { Inter } from 'next/font/google';
+import { ReactNode } from 'react';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'NetSphere',
+  description: 'Connect with same-minded people and share your ideas',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
